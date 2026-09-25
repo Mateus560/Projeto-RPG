ea81fcb36c95e1dce9544e5c53f92c9822e3dfcc
Mateus <103335412+Mateus560@users.noreply.github.com>
2026-09-25 14:59:00 -0300

    Corrigindo importação

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ import classes from "./data/classes.js";
 import almas from "./data/almas.js";
 import periciasDisponiveis from "./data/pericias.js";
 
+import {
+    exportarFicha,
+    importarFicha
+} from "./modules/backup.js";
 
 import {
     carregarPericiasIniciais,
@@ -40,10 +44,6 @@ import {
     carregarFicha
 } from "./modules/ficha.js";
 
-import {
-    exportarFicha,
-    importarFicha
-} from "../modules/backup.js";
 // ============================================================
 // REFERÊNCIAS DO HTML
 // ============================================================
